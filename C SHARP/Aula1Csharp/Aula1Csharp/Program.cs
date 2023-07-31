9c9316aadc61c046e1340c1be19437fc2d3b47b8
Arthur Bitencourt <113964510+Thurvabitt@users.noreply.github.com>
2023-07-31 20:20:52 -0300

    Add files via upload

---
+++ C SHARP/Aula1Csharp/Aula1Csharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1Csharp
+{
+    class MainClass
+    {
+        public static void Main(string[] args) //operador de visibilidade (Atente-se á string com letra minuscula)
+        {
+            Console.WriteLine("Olá mundo! Projeto no Visual Studio!");
+            Console.WriteLine("A aplicação terminou, tecle Enter para finalizar");
+            Console.ReadLine();
+        }
+    }
+}
