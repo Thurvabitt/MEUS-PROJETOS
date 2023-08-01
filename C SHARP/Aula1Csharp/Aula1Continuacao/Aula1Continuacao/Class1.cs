d1ca910eaeca44fb302be9fd8402fb1911d8bbd2
Arthur Bitencourt <113964510+Thurvabitt@users.noreply.github.com>
2023-07-31 21:36:15 -0300

    Add files via upload

---
+++ C SHARP/Aula1Csharp/Aula1Continuacao/Aula1Continuacao/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1Continuacao
+{
+    class Class1
+    {
+        //Caixa eletrônico;
+        //Usario digita um valor e voce calcula quantas notas serão utilizadas;
+        //Considere que o caixa só tem nota de 100, 50, 20, 10 5 e 2;
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um valor a ser sacado: ");
+            int valor = int.Parse(Console.ReadLine());
+
+            int[] notasDisponiveis = { 100, 50, 20, 10, 5, 2 };
+            int[] quantidadeNotas = new int[notasDisponiveis.Length];
+
+            for (int i = 0; i < notasDisponiveis.Length; i++)
+            {
+                quantidadeNotas[i] = valor / notasDisponiveis[i];
+                valor %= notasDisponiveis[i];
+            }
+
+            Console.WriteLine("Quantidade de notas: ");
+            for (int i=0; i < notasDisponiveis.Length; i++)
+            {
+                Console.WriteLine($"{notasDisponiveis[i]}: {quantidadeNotas[i]}");
+            }
+        }
+    }
+}
---
+++ C SHARP/Aula1Csharp/Aula1Continuacao/Aula1Continuacao/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1VerificaParImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um valor");
+            //cast
+            double valorString = 12.9;
+            int valorInt = (int)valorString;
+
+            int valor = int.Parse(Console.ReadLine());
+
+            if(valor%2 == 0)
+            {
+                Console.WriteLine("É par!");
+            }
+            else
+            {
+                Console.WriteLine("É impar!");
+            }
+            Console.ReadLine();
+             
+        }
+    }
+}
