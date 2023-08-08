18fd86fe6bd5ac18298c308a165f6aac9996cf98
Arthur Bitencourt <113964510+Thurvabitt@users.noreply.github.com>
2023-08-07 21:32:22 -0300

    Add files via upload

---
+++ C SHARP/Aula1Csharp/Aula2/Orientação Objeto/ContaCorrente.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula2
+{
+    internal class ContaCorrente
+    {
+     public string titular;
+        public int numero;
+        public int agencia;
+        public double saldo;
+
+        public bool Sacar(double valor)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+
+            else
+            {
+                saldo -= valor;
+                return true;
+            }
+        }
+
+        public bool Depositar(double valor)
+        {
+            if (valor > 0)
+            {
+                this.saldo += valor;
+                return true;
+            }
+
+            else
+            {
+                Console.WriteLine("Depósito deve ser maior que 0.0");
+                return false;
+            }
+        }
+
+    }
+}
---
+++ C SHARP/Aula1Csharp/Aula2/Orientação Objeto/Program.cs
@@ -0,0 +1,24 @@
+﻿using Aula2;
+
+public class OrientacaoObjeto
+{
+    public static void Main(string[] args)
+    {
+        ContaCorrente c1 = new ContaCorrente();
+        c1.titular = "12345678900";
+            c1.saldo = 100;
+
+        c1.Depositar(-1);
+
+        Console.WriteLine("Saldo de " + c1.titular + " é " + c1.saldo);
+        c1.Depositar(200);
+
+        Console.WriteLine("Saldo de " + c1.titular + " é " + c1.saldo);
+        c1.Sacar(350);
+
+        Console.WriteLine("Saldo de " + c1.titular + " é " + c1.saldo);
+        c1.Sacar(350);
+    }
+
+
+}
