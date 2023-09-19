1d73960f6a9480237ab6c5a880674b85bdf13a50
Arthur Bitencourt <113964510+Thurvabitt@users.noreply.github.com>
2023-09-18 21:20:04 -0300

    Add files via upload

---
+++ C SHARP/Aula1Csharp/API/Data/Banco.cs
@@ -0,0 +1,20 @@
+﻿using AP.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AP.Data
+{
+    public class Banco : DbContext
+    {
+        //Construtor da classe
+        public Banco(DbContextOptions<Banco> options) : base(options)
+        {
+        }
+        DbSet<Produto> Produtos { get; set; } 
+
+
+
+
+
+
+    }
+}
---
+++ C SHARP/Aula1Csharp/API/Models/Produto.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AP.Models
+{
+    public class Produto
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "coloca um nome aí otário")]
+        public String Nome { get; set; }
+        public int Quantidade { get; set; }
+        public double ValorUnitario { get; set; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
